2db9533ea2b86223465a37ada0ce4fede7a73dae
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-13 18:33:00 -0400

    Moficacion

---
+++ Ej3/Ej3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ej3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calcular();
+            Console.ReadKey();
+        }
+
+        static void calcular()
+        {
+            
+            double perimetro = 0, lado = 0, cantidad = 0;
+
+           
+            Console.WriteLine("Encontrar el Perimetro  de un Poligono.");
+
+
+            Console.Write("Ingrese cantidad de lados del Poligono: ");
+            cantidad = Int32.Parse(Console.ReadLine());
+
+            
+            Console.Write("Ingrese el lados del Poligono: ");
+            
+            lado = Int32.Parse(Console.ReadLine());
+
+
+
+
+
+           
+
+            perimetro = cantidad * lado;
+
+     
+            Console.WriteLine("El Area del Poligono : " + perimetro);
+
+
+
+          
+        }
+     
+    }
+}
